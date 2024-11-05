0702120fc465ce4faa166e76b2da94d1102e081c
CarlosMastache1 <carlosmastache301@gmail.com>
2024-11-05 11:30:43 -0600

    whitenoise3

--- myplataform/settings.py
+++ myplataform/settings.py
@@ -54,11 +54,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 ROOT_URLCONF = 'myplataform.urls'
 
