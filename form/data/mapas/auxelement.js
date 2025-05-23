2ee1002607a480554ecd0596675ed6027cbb4469
LS-Oscar <l.s.oscar.2000@gmail.com>
2025-05-23 10:26:50 -0600

    Añadido de los geojson de Oaxaca dividido por municipios, de mexico devidido por estado, del mundo devidido por paises, js de cultivo de piña su superficie sembrada y volumen de produccion.

---
+++ form/data/mapas/auxelement.js
@@ -0,0 +1,36 @@
+const paisesDatos = {
+  Nigeria: [{ id: "NGA", territorio: "193,2900" }],
+  India: [{ id: "IND", territorio: "108,000" }],
+  China: [{ id: "CHN", territorio: "76,723" }],
+  Tailandia: [
+    { id: "THA", territorio: "71,518" },
+  ],
+  Filipinas: [
+    { id: "PHL", territorio: "67,722" },
+  ],
+  Brasil: [{ id: "BRA", territorio: "64,147" }],
+  "Costa Rica": [
+    { id: "CRI", territorio: "40,000" },
+  ],
+  Vietnam: [
+    { id: "VNM", territorio: "39,647" },
+  ],
+  Angola: [
+    { id: "AGO", territorio: "36,946" },
+  ],
+  "México": [
+    { id: "MEX", territorio: "25,626" },
+  ],
+  Indonesia: [
+    { id: "IDN", territorio: "24,327" },
+  ],
+  Colombia: [
+    { id: "COL", territorio: "22,870" },
+  ],
+  Venezuela: [
+    { id: "VEN", territorio: "22,173" },
+  ],
+  "Perú": [
+    { id: "PER", territorio: "16,830" },
+  ],
+};
---
+++ form/data/mapas/pina.js
@@ -0,0 +1,143 @@
+const regionesConMunicipios = {
+  Papaloapan: [
+    { id: 20044, territorio: "1554", toneladas: "129,374.25" },
+    { id: 20184, territorio: "610", toneladas: "48,190" },
+  ],
+  Istmo: [{ id: 20198, territorio: "12.95", toneladas: "146.63" }],
+  "Sierra Sur": [
+    { id: 20516, territorio: "4.55", toneladas: "81.28" },
+    { id: 20448, territorio: "4.3", toneladas: "71.58" },
+    { id: 20229, territorio: "3.2", toneladas: "52.78" },
+  ],
+};
+
+const estadosDatos = {
+  Veracruz: [
+    {
+      id: 30,
+      territorio: "38,642",
+      toneladas: "840,317",
+    },
+  ],
+
+  Oaxaca: [
+    {
+      id: 20,
+      territorio: "2,189",
+      toneladas: "177,917",
+    },
+  ],
+
+  Nayarit: [
+    {
+      id: 18,
+      territorio: "2,018",
+      toneladas: "61,739",
+    },
+  ],
+
+  Tabasco: [
+    {
+      id: 27,
+      territorio: "1,680",
+      toneladas: "61,040",
+    },
+  ],
+
+  "Quintana Roo": [
+    {
+      id: 23,
+      territorio: "892",
+      toneladas: "52,618",
+    },
+  ],
+
+  Colima: [
+    {
+      id: 6,
+      territorio: "472",
+      toneladas: "36,131",
+    },
+  ],
+
+  Jalisco: [
+    {
+      id: 14,
+      territorio: "554",
+      toneladas: "32,133",
+    },
+  ],
+
+  Chiapas: [
+    {
+      id: 7,
+      territorio: "368",
+      toneladas: "7,972",
+    },
+  ],
+
+  Campeche: [
+    {
+      id: 4,
+      territorio: "43",
+      toneladas: "1,030",
+    },
+  ],
+
+  Tamaulipas: [
+    {
+      id: 28,
+      territorio: "19",
+      toneladas: "563",
+    },
+  ],
+
+  Guerrero: [
+    {
+      id: 12,
+      territorio: "47",
+      toneladas: "504",
+    },
+  ],
+
+  Hidalgo: [
+    {
+      id: 13,
+      territorio: "8",
+      toneladas: "349",
+    },
+  ],
+
+  México: [
+    {
+      id: 15,
+      territorio: "15",
+      toneladas: "128",
+    },
+  ],
+
+  Yucatán: [
+    {
+      id: 31,
+      territorio: "8",
+      toneladas: "121",
+    },
+  ],
+};
+
+const paisesDatos = {
+  Nigeria: [{ id: "NGA", territorio: "193,2900" }],
+  India: [{ id: "IND", territorio: "108,000" }],
+  China: [{ id: "CHN", territorio: "76,723" }],
+  Tailandia: [{ id: "THA", territorio: "71,518" }],
+  Filipinas: [{ id: "PHL", territorio: "67,722" }],
+  Brasil: [{ id: "BRA", territorio: "64,147" }],
+  "Costa Rica": [{ id: "CRI", territorio: "40,000" }],
+  Vietnam: [{ id: "VNM", territorio: "39,647" }],
+  Angola: [{ id: "AGO", territorio: "36,946" }],
+  México: [{ id: "MEX", territorio: "25,626" }],
+  Indonesia: [{ id: "IDN", territorio: "24,327" }],
+  Colombia: [{ id: "COL", territorio: "22,870" }],
+  Venezuela: [{ id: "VEN", territorio: "22,173" }],
+  Perú: [{ id: "PER", territorio: "16,830" }],
+};
