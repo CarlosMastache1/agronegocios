60a552bbca4d63b59c5975084ca4438ecaee3280
CarlosMastache1 <carlosmastache301@gmail.com>
2025-09-23 09:20:17 -0600

    csrf token acrivado allowed

--- myplataform/settings.py
+++ myplataform/settings.py
@@ -95,32 +95,32 @@ WSGI_APPLICATION = 'myplataform.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 # BASE DE DATOS PARA PRODUCCION
-# DATABASES = {
-#                              'default': {
-#                                  'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#                                  'NAME': 'agronegocios',
-#                                  'USER': 'carlos',
-#                                  'PASSWORD': 'mastache123',
-#                                  'HOST': 'agronegocios.c3k440iemp4z.us-west-1.rds.amazonaws.com',
-#                                  'PORT': '5432', 
-#                                  'OPTIONS' : {
-#                                             'sslmode': 'require'
-#                                         },
-#                                         'CONN_MAX_AGE': 60,
-#                                              }
-#                              }
+DATABASES = {
+                              'default': {
+                                  'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                                  'NAME': 'agronegocios',
+                                  'USER': 'carlos',
+                                  'PASSWORD': 'mastache123',
+                                  'HOST': 'agronegocios.c3k440iemp4z.us-west-1.rds.amazonaws.com',
+                                  'PORT': '5432', 
+                                  'OPTIONS' : {
+                                             'sslmode': 'require'
+                                         },
+                                         'CONN_MAX_AGE': 60,
+                                              }
+                              }
 
 # BASE DE DATOS PARA DESARROLLO
-DATABASES = {
-                                                                                'default': {
-                                                                                 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-                                                                                 'NAME': 'developer',
-                                                                                 'USER': 'postgres', 
-                                                                                 'PASSWORD': 'mastache', 
-                                                                                 'HOST': 'localhost',
-                                                                                 'PORT': '5432'
-                                                                             }
-                                                                          } 
+# DATABASES = {
+#                                                                                 'default': {
+#                                                                                  'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#                                                                                  'NAME': 'developer',
+#                                                                                  'USER': 'postgres', 
+#                                                                                  'PASSWORD': 'mastache', 
+#                                                                                  'HOST': 'localhost',
+#                                                                                  'PORT': '5432'
+#                                                                              }
+#                                                                           } 
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
